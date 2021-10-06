26ddadddc78b1ff26cebc7041085f08f9d4af43c
ascolltd <stephencuddy16@hotmail.com>
2021-10-06 20:06:30 +0100

    commit

---
+++ test.js
@@ -0,0 +1,37 @@
+console.log('tetsteljgqfugi');
+//variables
+//cannot be reserved keyword
+//should be meaningful
+//cannot start with a number 
+//must not contain space or hyphen
+
+let firstname = 'stephen';
+let firstname2 = 'alan';
+let lastname = 'jobs';
+let lasrname2 = 'cuddy';
+let age = 27;
+let age2 = 27;
+let isapp = true;
+console.log(firstname,lastname,firstname2,lasrname2);
+
+let person = {
+    name: 'Alan',
+    age: 20,
+    hair: 'noner'
+};
+console.log(person);
+person.age = 50;
+console.log(person.age);
+person['age'] = 60;
+
+let selction =['erw','sfd','isuv'];
+selction[1] =6494;
+console.log(selction);
+
+function greet(name1){
+    console.log('hello world',name1);
+    console.log('argument is what is passed to the variable parameter it what takes the variable')
+
+}
+
+greet('tommy')
